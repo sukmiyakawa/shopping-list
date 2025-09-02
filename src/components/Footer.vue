d8f24505fa9d280390918339d40ec038d7d7e390
Suguru Miyakawa <suk.web96@gmail.com>
2025-09-03 08:17:57 +0900

    lang属性を日本語化

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <small>&copy;2025&ensp;SuguruMiyakawa</small>
+</template>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>shopping-list</h1>
+</template>
+
+<style scoped></style>
